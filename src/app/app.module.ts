5c6ad9ac1bd0e5e0bf44ab7819fc229ddb550a89
vcrolack <victorolack@gmail.com>
2022-03-11 17:23:28 -0300

    UI: ui formulario terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
